7e0ed05f757a7b71cc7cc92c3b03dd869ce3d5d0
Matt Areddia <mareddia@protonmail.com>
2019-12-21 23:54:04 -0600

    Configure splitChunks in storybook webpack.config.js

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,6 @@
+const merge = require('webpack-merge');
+const maxAssetSize = 1024 * 1024;
+
 module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.story\.tsx?$/,
@@ -19,5 +22,16 @@ module.exports = async ({ config }) => {
   
   config.resolve.extensions.push('.ts', '.tsx');
 
-  return config;
+ return merge(config, {
+   optimization: {
+     splitChunks: {
+       chunks: 'all',
+       minSize: 30 * 1024,
+       maxSize: maxAssetSize,
+     },
+   },
+   performance: {
+     maxAssetSize: maxAssetSize,
+   },
+ });
 };
