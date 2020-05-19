7272cbb776e9aa6988f0a28a01b705b8fd52328e
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-19 15:09:18 +0100

    Updated tests set up to allow for non js-dom envrionment tests
    When jest-environment is set to `node` window is not defined and the mutation observer polyfil cannot be added.

--- .test/setupTests.ts
+++ .test/setupTests.ts
@@ -1,18 +1,21 @@
 /* eslint-disable prettier/prettier */
 import '@testing-library/jest-dom/extend-expect';
-import MutationObserver from 'mutation-observer';
 
-// Global MutationObserver Mocks
-window.MutationObserver = MutationObserver;
+// React SSR when environment is not js-dom
+if (global.window !== undefined) {
 
-// Global Test Environment Mocks
-window.matchMedia = jest.fn((query: string): MediaQueryList => ({
-  media: query,
-  onchange: null,
-  matches: false,
-  addListener: jest.fn(),
-  dispatchEvent: jest.fn(),
-  removeListener: jest.fn(),
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn()
-}));
+  // Global MutationObserver Mocks
+  window.MutationObserver = require('mutation-observer');
+
+  // Global Test Environment Mocks
+  window.matchMedia = jest.fn((query: string): MediaQueryList => ({
+    media: query,
+    onchange: null,
+    matches: false,
+    addListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn()
+  }));
+}
