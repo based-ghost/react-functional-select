58d10fc0d4f7fabd21d7400a8d0c247851424b39
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-19 14:49:59 +0100

    Updated useIsTouchDevice to work when rendering with SSR
    Server side rendering causes an error when rendering a component using this hook when it looks for `navigator`. Added checks for `navigator` and `window` to avoid the look up.

--- src/hooks/useIsTouchDevice.ts
+++ src/hooks/useIsTouchDevice.ts
@@ -8,6 +8,12 @@ export const useIsTouchDevice = (): boolean => {
   const isTouchDevice = useRef<boolean | null>(null);
 
   if (isTouchDevice.current === null) {
+    // Check for SSR where navigator and window may not be defined.
+    if (navigator === undefined || window === undefined) {
+      isTouchDevice.current = false;
+      return isTouchDevice.current;
+    }
+
     isTouchDevice.current = ('ontouchstart' in window || !!navigator.maxTouchPoints);
     return isTouchDevice.current;
   }
