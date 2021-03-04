b3f8666898d4943b87d27fccdcf8ca66b99cbff8
Matt Areddia <mareddia@protonmail.com>
2021-03-03 18:59:00 -0600

    update storybook configuration in main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,19 +21,6 @@ module.exports = {
     reactDocgen: 'none'
   },
   webpackFinal: async (config: Configuration) => {
-    /*config.module.rules.push({
-      test: /\.stories\.tsx?$/,
-      loaders: [
-        {
-          loader: require.resolve('@storybook/source-loader'),
-          options: {
-            parser: 'typescript',
-          },
-        },
-      ],
-      enforce: 'pre',
-    }); */
-
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve('babel-loader'),
