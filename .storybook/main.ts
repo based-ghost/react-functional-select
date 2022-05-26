7c599edbd2176716bc17b1b38918b44f56408e4f
Matt Areddia <mareddia@protonmail.com>
2022-05-25 19:45:31 -0500

    update to storybook config - main.ts (remove extraneous mapping to static public dir)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,14 @@ import type { StorybookConfig } from '@storybook/react/types';
 const config: StorybookConfig = {
   stories: ['../__stories__/**/*.stories.@(js|tsx|mdx)'],
   addons: ['@storybook/addon-storysource'],
+  framework: '@storybook/react',
   reactOptions: {
     fastRefresh: true
   },
   core: {
     builder: 'webpack5',
     disableTelemetry: true
-  },
-  staticDirs: ['../public'],
-  framework: '@storybook/react'
+  }
 };
 
 module.exports = config;
