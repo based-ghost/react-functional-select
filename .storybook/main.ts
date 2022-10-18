8670e1c9d8835f71a794c20a27eb0e8d4322af2b
Matt Areddia <mareddia@protonmail.com>
2022-10-17 19:46:50 -0500

    bump node deps/dev deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,12 +4,15 @@ const config: StorybookConfig = {
   stories: ['../__stories__/**/*.stories.@(js|tsx|mdx)'],
   addons: ['@storybook/addon-storysource'],
   framework: '@storybook/react',
-  reactOptions: {
-    fastRefresh: true
-  },
   core: {
-    builder: 'webpack5',
-    disableTelemetry: true
+    disableTelemetry: true,
+    enableCrashReports: false,
+    builder: {
+      name: 'webpack5',
+      options: {
+        lazyCompilation: false // applies to dev mode only
+      }
+    }
   }
 };
 
